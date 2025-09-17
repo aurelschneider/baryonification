4ca69db6612877fe5ce11fe7373d9ad888c08abf
Jozef Bucko <joz.bucko@gmail.com>
2025-09-17 14:31:47 +0200

    removed build and .egg folders

--- build/lib/baryonification/__init__.py
+++
@@ -1,6 +0,0 @@
-from .params import par
-from .constants import *
-from .profiles import Profiles
-from .cosmo import CosmoCalculator
-from .displ import ParticleDisplacer
-from .halo_displ import HaloDisplacer
